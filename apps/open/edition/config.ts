hpbrn/creed

release open 1.0.0

apps/open/edition/config.ts
@@ -1,0 +1,26 @@
+import type { CreedEdition } from "@/lib/edition";
+
+export const edition = {
+  kind: "open",
+  capabilities: {
+    hostedAccounts: false,
+    sharedCreeds: false,
+    managedBilling: false,
+    managedCredits: false,
+    feedback: false,
+    cli: false,
+    publicSignup: false,
+  },
+  routes: {
+    unauthenticated: "/claim",
+    connectAuthentication: "/claim",
+  },
+  save: {
+    persistedLabel: "Saved to database",
+    persistedTone: "text-[#16A34A] dark:text-[#4ADE80]",
+    pendingLabel: "Saving…",
+    pendingTone: "text-[var(--creed-text-secondary)]",
+    failureLabel: "Couldn’t save",
+    icon: "database",
+  },
+} as const satisfies CreedEdition;

connorhpbrn/my-creed

apps/open/edition/config.ts
@@ -1,0 +1,26 @@
+import type { CreedEdition } from "@/lib/edition";
+
+export const edition = {
+  kind: "open",
+  capabilities: {
+    hostedAccounts: false,
+    sharedCreeds: false,
+    managedBilling: false,
+    managedCredits: false,
+    feedback: false,
+    cli: false,
+    publicSignup: false,
+  },
+  routes: {
+    unauthenticated: "/setup",
+    connectAuthentication: "/setup",
+  },
+  save: {
+    persistedLabel: "Saved to database",
+    persistedTone: "text-[#16A34A] dark:text-[#4ADE80]",
+    pendingLabel: "Saving…",
+    pendingTone: "text-[var(--creed-text-secondary)]",
+    failureLabel: "Couldn’t save",
+    icon: "database",
+  },
+} as const satisfies CreedEdition;